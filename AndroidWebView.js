e400f190101a49800ea632b4c84654ee19dec344
Koh Peng How <penghow.koh@ifastfinancial.com>
2017-12-06 20:17:37 +0800

    --

--- AndroidWebView.js
+++ AndroidWebView.js
@@ -237,6 +237,15 @@ class AndroidWebView extends Component {
      */
     saveFormDataDisabled: PropTypes.bool,
 
+    /*
+     * Used on Android only, controls whether the given list of URL prefixes should
+     * make {@link com.facebook.react.views.webview.ReactWebViewClient} to launch a
+     * default activity intent for those URL instead of loading it within the webview.
+     * Use this to list URLs that WebView cannot handle, e.g. a PDF url.
+     * @platform android
+     */
+    urlPrefixesForDefaultIntent: PropTypes.arrayOf(PropTypes.string),
+
     /**
      * Make upload file available
      */
@@ -421,6 +430,7 @@ class AndroidWebView extends Component {
         allowUniversalAccessFromFileURLs={this.props.allowUniversalAccessFromFileURLs}
         mixedContentMode={this.props.mixedContentMode}
         saveFormDataDisabled={this.props.saveFormDataDisabled}
+        urlPrefixesForDefaultIntent={this.props.urlPrefixesForDefaultIntent}
         uploadEnabledAndroid={true}
       />
     );
